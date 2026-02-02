37486924bc405cc56a186340b05148f61a8ce49e
UN <youen1126@gmail.com>
2026-02-02 17:28:17 +0800

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,6 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import App from './App.jsx'
-import Week2 from './Week2.jsx'
 import Week5 from './Week5.jsx';
 //import Week4 from './weekWork/Week4.jsx';
 
