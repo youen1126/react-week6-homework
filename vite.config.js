7373836cc528852f0b415d917ddd2647e2e3efe4
UN <youen1126@gmail.com>
2026-02-24 15:09:12 +0800

    week6-gh-page

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-week5-homework/',
+  base: '/react-week6-homework/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
